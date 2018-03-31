86ce12cc64d5582bbd498f212cc8d5eaab8e5e50
b <f.balazs96+github@gmail.com>
2018-03-31 12:32:03 -0400

    initial

---
+++ MonitorSoundAuto/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ MonitorSoundAuto/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 40; i++)
+            {
+                freqs[i] = 1000 * Math.Exp(i / 16.0);
+            }
+        }
+        double[,] table;
+        int table_w = 0, table_h = 0;
+        double[] freqs = new double[40];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+            Location = new Point(0, 0);
+            Size = Screen.FromControl(this).Bounds.Size;
+            Bitmap bmp = Properties.Resources.wave;
+            table_w = bmp.Width;
+            table_h = bmp.Height;
+            table = new double[table_w, table_h];
+            for (int x = 0; x < table_w; x++)
+            {
+                for (int y = 0; y < table_h; y++)
+                {
+                    table[x, y] = bmp.GetPixel(x, y).R / 255f;
+                }
+            }
+            timer2.Start();
+            sw.Restart();
+            Cursor.Hide();
+        }
+        Queue<int> q = new Queue<int>();
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Close();
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.White);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            const double FRAME_TIME = 1.0 / 60.0;
+            double VERTICAL_SIZE = Size.Height;
+            int y = 0;
+            while (y < VERTICAL_SIZE)
+            {
+                float op = 0;
+                float total_weight = 0;
+                for (int i = 0; i < table_h; i++)
+                {
+                    double wave_time = 1.0 / freqs[table_h - 1 - i];
+                    double wavelength = wave_time / FRAME_TIME * VERTICAL_SIZE;
+                    int frame = (int)(sw.ElapsedMilliseconds / 100f);
+                    op += (float)(Math.Sin(y / wavelength * 2 * Math.PI + frame % 10 + 10) * table[frame % table_w, i]);
+                    total_weight += (float)table[frame % table_w, i];
+                }
+                op /= (total_weight == 0 ? 1 : total_weight);
+                op *= 10;
+                op += 1;
+                op /= 2;
+                op *= 255;
+                op = Math.Min(255, Math.Max(0, op));
+                e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb((int)op, Color.Black)), 0, y, Size.Width, 1);
+                y++;
+            }
+        }
+        Stopwatch sw = new Stopwatch();
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
